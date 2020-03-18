c2302355e9b56d66974e6861e1fec54c6316f062
kimjungwon <koo_m@naver.com>
2020-03-18 11:27:54 +0000

    Update Day08/BlockChainStarter.java

---
+++ Day08/BlockChainStarter.java
@@ -0,0 +1,16 @@
+package core;
+
+import util.Util;
+
+public class BlockChainStarter {
+	public static void main(String[] args) {
+		int nonce = 0;
+		while (true) {
+			if (Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
+				System.out.println("Á¤´ä: " + nonce);
+				break;
+			}
+			nonce++;
+		}
+	}
+}
